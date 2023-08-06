6197afcfc6493c0e8187b612ea847984c65a5688
unknown <jonas.dally@hotmail.de>
2023-08-06 16:10:15 +0200

    adding text-decoration

--- my-app/pages/Subpages/[DiyNotes].js
+++ my-app/pages/Subpages/[DiyNotes].js
@@ -7,15 +7,10 @@ import useLocalStorageState from "use-local-storage-state";
 
 export default function DiyNotesRouter({ cards }) {
   const router = useRouter();
-
   const [dates, setDates] = useLocalStorageState("dates", { defaultValue: [] });
-
   const currentId = router.query.DiyNotes;
-
   const currentCards = cards.filter((card) => card.id === currentId);
-
   const filteredDates = dates.filter((date) => date.parentID === currentId);
-  console.log(filteredDates);
 
   function handleSubmitCardNote(event) {
     event.preventDefault();
@@ -23,11 +18,36 @@ export default function DiyNotesRouter({ cards }) {
     const inputNoteData = {
       id: uid(),
       parentID: currentId,
+      isDone: false,
       name: event.target.notiz.value,
     };
     setDates([...dates, inputNoteData]);
     event.target.reset();
   }
+
+  function handleCheckboxToggle(id) {
+    const checked = dates.find((date) => date.id === id);
+    if (checked) {
+      setDates(
+        dates.map((date) =>
+          date.id === id ? { ...date, isDone: !date.isDone } : date
+        )
+      );
+    }
+  }
+
+  console.log(dates);
+  /*if (artPieceInfo) {
+    setArtPiecesInfo(
+      artPiecesInfo.map((piece) =>
+        piece.slug === slug ? { slug, isFavorite: !piece.isFavorite } : piece
+      )
+    );
+  } else {
+    setArtPiecesInfo([...artPiecesInfo, { slug, isFavorite: true }]);
+  }
+}*/
+
   return (
     <>
       <Header />
@@ -42,6 +62,7 @@ export default function DiyNotesRouter({ cards }) {
                 name="notiz"
                 rows="5"
                 placeholder="type your note here..."
+                required
               ></textarea>
               <button type="submit">Notiz hinzufügen</button>
             </StyledFormNoteDiv>
@@ -49,18 +70,26 @@ export default function DiyNotesRouter({ cards }) {
           </StyledDiyNoteDiv>
         </>
       ))}
-      {filteredDates.map((data) => (
-        <>
-          <p key={data.id}>{data.name}</p>
-        </>
-      ))}
+      <StyledUl>
+        {filteredDates.map((data) => (
+          <>
+            <StyledListItem>
+              <p key={data.id}>{data.name}</p>
+              <input
+                type="checkbox"
+                onClick={() => handleCheckboxToggle(data.id)}
+              ></input>
+            </StyledListItem>
+          </>
+        ))}
+      </StyledUl>
 
       <Navbar />
     </>
   );
 }
 
-const StyledDiyNoteDiv = styled.div`
+const StyledDiyNoteDiv = styled.section`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -76,3 +105,27 @@ const StyledFormNoteDiv = styled.form`
   text-align: center;
   align-items: center;
 `;
+
+const StyledUl = styled.ul`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 1rem;
+  align-items: flex-end;
+`;
+
+const StyledListItem = styled.li`
+  text-align: center;
+  display: flex;
+  gap: 1rem;
+  border: 1px solid black;
+  border-top-left-radius: 200px;
+  border-bottom-left-radius: 200px;
+  padding: 2rem;
+`;
+
+/* text-decoration: ${({ $ischecked }) =>
+    $ischecked ? "line-through" : "none"};*/
+
+/* ${({ $isOnFire }) => ($isOnFire ? "🔥" : "❄️")};*/
